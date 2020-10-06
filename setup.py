199b91a66ff1d394aeced6af70dfe5d1d993870a
jvs <vonseg@protonmail.com>
2020-10-06 05:52:23 -0500

    Increment version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def long_description():
 
 setup(
     name = 'sourcer',
-    version = '0.3.0',
+    version = '0.3.1',
     author = 'John K. Von Seggern',
     author_email = 'vonseg@protonmail.com',
     url = 'https://github.com/jvs/sourcer',
